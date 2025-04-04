a109d14d9410b914829a9cf47751b8821a05aeee
Damilola Ogunboyejo <demilademichael18@gmail.com>
2025-04-04 09:23:45 +0100

    added-twitter-tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,8 @@
 /* eslint-disable @next/next/next-script-for-ga */
 import Document, {
   Html, Head, Main, NextScript
-} from 'next/document';
-import Script from 'next/script';
+} from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -12,9 +12,19 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <meta name="description" content="Shield yourself from online scams, protect your transactions with UseBridge Inc." />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <meta
+            name="description"
+            content="Shield yourself from online scams, protect your transactions with UseBridge Inc."
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
@@ -39,6 +49,35 @@ class MyDocument extends Document {
                   page_path: window.location.pathname,
                 });
               `
+            }}
+          />
+
+          <Script
+            id="twitter-base"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(e,t,n,s,u,a){
+              e.twq||(s=e.twq=function(){
+                s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))
+            }(window,document,'script');
+            twq('config','pfu9z');
+          `
+            }}
+          />
+
+          <Script
+            id="twitter-event"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            twq('event', 'tw-pfu9z-pfua1', {
+              email_address: null,
+              phone_number: null
+            });
+          `
             }}
           />
         </body>
