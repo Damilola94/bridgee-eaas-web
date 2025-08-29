bc39177811a0b688b1ae9d84b58134f1d7775c85
Tosin <rotimidokun@gmail.com>
2025-08-29 11:53:31 +0100

    rebase - integrate seller-signup-apis

---
+++ src/types/bank.ts
@@ -0,0 +1,19 @@
+export interface Bank {
+  bankCode: string;
+  bankName: string;
+}
+
+export interface BanksApiResponse {
+  isSuccess: boolean;
+  statusCode: string;
+  message: string;
+  data: Bank[];
+  metaData: any | null;
+}
+
+export interface AddLinkedBankData {
+  bankCode: string;
+  accountNumber: string;
+  accountName: string;
+  isPrimary: boolean;
+}
