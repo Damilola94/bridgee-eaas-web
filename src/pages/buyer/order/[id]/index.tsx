28be44319deef0c35644786380d482428dbc5db3
Damilola <demilademichael18@gmail.com>
2026-01-23 15:06:12 +0100

    fix:fix-bug-with-make-payment

--- src/pages/buyer/order/[id]/index.tsx
+++ src/pages/buyer/order/[id]/index.tsx
@@ -190,7 +190,7 @@ export default function BuyerOrder() {
   //   return <Loading />;
   // }
 
-  const allowPayment = !orderStatusData?.data?.allowPayment;
+  const allowPayment = orderStatusData?.data?.allowPayment;
 
   if (allowPayment === true) {
     if (!showPaymentView) {
@@ -201,7 +201,6 @@ export default function BuyerOrder() {
           </Head>
           <div className="min-h-screen bg-gray-50 flex flex-col justify-center px-4">
             <div className="container mx-auto">
-
               {/* Mobile Header */}
               <div className="lg:hidden bg-white border-b border-gray-200 w-full px-4 py-4 mb-4">
                 <div className="flex items-center">
